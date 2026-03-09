1b3b4bfabf72ecaafc8d5f494f737e87bff5f405
mir-md-rahim <mirmdrahim24@gmail.com>
2026-03-09 20:40:42 +0600

    fetch all issues data

--- script/dashboard.js
+++ script/dashboard.js
@@ -6,3 +6,37 @@ const btnClosed = document.getElementById("btnClosed");
 const totalIssues = document.getElementById("totalIssues");
 const allItemContainer = document.getElementById("allItemContainer");
 const loadingBar = document.getElementById("loadingBar");
+
+let allIssues = [];
+
+console.log(
+  searchBtn,
+  searchInput,
+  btnAll,
+  btnOpen,
+  btnClosed,
+  totalIssues,
+  allItemContainer,
+  loadingBar,
+);
+
+function showLoadingBar(isLoading) {
+  if (isLoading) {
+    loadingBar.classList.remove("hidden");
+    allItemContainer.classList.remove("hidden");
+  } else {
+    loadingBar.classList.add("hidden");
+    allItemContainer.classList.remvoe("hidden");
+  }
+}
+
+async function allItemLoad() {
+  const res = await fetch(
+    "https://phi-lab-server.vercel.app/api/v1/lab/issues",
+  );
+  const data = await res.json();
+  allIssues = data.data;
+  displayAllItem(allIssues);
+}
+
+allItemLoad();
